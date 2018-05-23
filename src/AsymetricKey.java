f03758a2abc7981c1cd29d32e0b2a6a1c14f7f92
Zaira Rubio Lázaro <zrubio@protonmail.com>
2018-05-23 17:24:36 +0200

    Commit

--- src/AsymetricKey.java
+++ src/AsymetricKey.java
@@ -1,8 +1,45 @@
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
 public class AsymetricKey {
 
     public static void main(String[] args) {
 
+        try{
+            // Obtener la instancia asignandole el algoritmo que vamos a utilizar
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+            keyPairGenerator.initialize(512);
+            KeyPair keyPair = keyPairGenerator.generateKeyPair();
 
+            // Obtener clave publica
+            PublicKey publicKey = keyPair.getPublic();
+            // Obtener clave privada
+            PrivateKey privateKey = keyPair.getPrivate();
+
+            String mensaje = "Hola mundo";
+            byte[] mensajeBytes = mensaje.getBytes();
+
+            // Encriptacion con clave publica
+            Cipher cipher = Cipher.getInstance("RSA");
+            cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+            byte[] mensajeEncriptadoBytes = cipher.doFinal(mensajeBytes);
+
+            System.out.println("Mensaje original: "+ new String(mensajeBytes));
+            System.out.println("Mensaje encriptado: "+new String(mensajeEncriptadoBytes));
+
+            // Desencriptacion con clave privada
+            cipher.init(Cipher.DECRYPT_MODE, privateKey);
+            byte[] mensajeDesencriptadoBytes = cipher.doFinal(mensajeEncriptadoBytes);
+
+            System.out.println("Mensaje desencriptado: "+new String(mensajeDesencriptadoBytes));
+
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
 
     }
 
